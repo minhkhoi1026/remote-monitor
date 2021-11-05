108095ad0b0411b401eade440a7ba81d671f496b
rainbowdango <minhkhoi1026@gmail.com>
2021-11-05 10:17:47 +0700

    add get document dir path utility

--- file_management.py
+++ file_management.py
@@ -10,6 +10,10 @@ from enum import Enum
 import time
 import string
 from ctypes import windll
+from win32com.shell import shell, shellcon
+
+def get_document_path():
+    return shell.SHGetFolderPath(0, shellcon.CSIDL_PERSONAL, None, 0)
 
 def get_drives():
     drives = []
