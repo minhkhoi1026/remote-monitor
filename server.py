65858c31c2aea627d257f6a3ee8721c9c249ab90
trungtruc2357 <69811172+trungtruc2357@users.noreply.github.com>
2021-11-06 10:27:57 +0700

    add title and icon, server add choose IP, client add change button text

--- server.py
+++ server.py
@@ -1,33 +1,9 @@
-#install pyqt5:
-#                       pip install pyqt5
-#                       pip install pyqt5-tools
-
-#command make python file:
-#                       pyuic5 -x Client.ui -o ClientGui.py
-#                       pyuic5 -x Server.ui -o ServerGui.py
-#                       pyrcc5 picture.qrc -o picture_rc.py
-
-#run app:
-#                       python ClientGui.py
-#                       python main.py
-#        & C:/Users/Dell/AppData/Local/Microsoft/WindowsApps/python3.10.exe
-#        & C:/Users/Dell/AppData/Local/Microsoft/WindowsApps/pip3.10.exe install
-
-#run server
-# & C:/Users/Dell/AppData/Local/Microsoft/WindowsApps/python3.10.exe d:/Subject/nam2_hk2/Mang_may_tinh/cuoiki_socket/mybotnet/server_socket.py
-#run client
-# & C:/Users/Dell/AppData/Local/Microsoft/WindowsApps/python3.10.exe d:/Subject/nam2_hk2/Mang_may_tinh/cuoiki_socket/mybotnet/client_socket.py
-
-#open Designer:
-#               C:\Users\Dell\AppData\Roaming\Python\Python37\site-packages\qt5_applications\Qt\bin
-
-
 import sys
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox, QLabel, QSizePolicy
-from PyQt5.QtGui import QImage, QPixmap, QPalette, QPainter
+from PyQt5 import QtCore
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from ServerGui import Ui_MainWindow
-from server_socket import SocketServer
+from server_socket import SocketServer, get_all_avail_host
 from streaming import StreamingServer
 
 
@@ -36,26 +12,38 @@ class ServerWindow:
         self.main_win = QMainWindow()
         self.uic = Ui_MainWindow()
         self.uic.setupUi(self.main_win)
+        _translate = QtCore.QCoreApplication.translate
+        self.main_win.setWindowTitle(_translate("MainWindow", "Server"))
+        self.main_win.setWindowIcon(QIcon("image\\icon_window.png"))
+
         self.socket = SocketServer()
         self.stream_socket = StreamingServer()
         self.uic.button_start.clicked.connect(self.start)
         self.uic.button_stop.clicked.connect(self.stop)
-        self.uic.button_stop.setEnabled(False)
+        self.uic.button_stop.hide()
+        self.add_item_IP()
 
     def show(self):
         self.main_win.show()
 
+    def add_item_IP(self):
+        self.uic.comboBox_IP.addItems(get_all_avail_host())
+
     def start(self):
-        self.socket.start_server('127.0.0.1', 26100)
-        self.stream_socket.start_server('127.0.0.1', 9999)
-        self.uic.button_start.setEnabled(False)
-        self.uic.button_stop.setEnabled(True)
+        IP = self.uic.comboBox_IP.currentText()
+        print(IP)
+        self.socket.start_server(IP, 26100)
+        self.stream_socket.start_server(IP, 9999)
+        self.uic.comboBox_IP.setEnabled(False)
+        self.uic.button_start.hide()
+        self.uic.button_stop.show()
 
     def stop(self):
         self.socket.stop_server()
         self.stream_socket.stop_server()
-        self.uic.button_start.setEnabled(True)
-        self.uic.button_stop.setEnabled(False)
+        self.uic.comboBox_IP.setEnabled(True)
+        self.uic.button_start.show()
+        self.uic.button_stop.hide()
 
 
 if __name__ == "__main__":
@@ -65,6 +53,3 @@ if __name__ == "__main__":
     sys.exit(app.exec())
 
     
-
-
-
